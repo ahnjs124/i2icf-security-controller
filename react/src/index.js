2f97c132c448dbc254389ca62d416a28981773c2
ahnjs124 <coffeecat432@gmail.com>
2025-09-15 17:17:03 +0900

    test

--- react/src/index.js
+++ react/src/index.js
@@ -4,16 +4,6 @@ import App from './App';
 
 import './components/navbar.css'
 
-
-
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
-
-
 ReactDOM.render(
   <React.StrictMode>
     <App />
