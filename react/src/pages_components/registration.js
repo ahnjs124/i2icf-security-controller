2f24b406230365788689a82feded95b6ab4cb955
ahnjs124 <coffeecat432@gmail.com>
2025-09-15 19:38:33 +0900

    commit

--- react/src/pages_components/registration.js
+++ react/src/pages_components/registration.js
@@ -13,38 +13,42 @@ import './registration.css'
 export default function Registration(props) {
 
   // variables for user group modal
-  const [openModal, setOpenModal] = useState(false); // 모달 창 열림 상태 관리
+  const [openModal, setOpenModal] = useState(false); // UserGroup 창 열림/닫힘 관리 상태 변수
   
   // variables for device group modal
-  const [openDeviceModal, setOpenDeviceModal] = useState(false); // 모달 창 열림 상태 관리
+  const [openDeviceModal, setOpenDeviceModal] = useState(false); // DeviceGroup 창 열림/닫힘 관리 상태 변수
 
   // variables for the location group modal
-  const [openLocationModal, setOpenLocationModal] = useState(false); // 모달 창 열림 상태 관리
+  const [openLocationModal, setOpenLocationModal] = useState(false); // LocationGroup 창 열림/닫힘 관리 상태 변수
 
   // variables for the URL group modal
-  const [openUrlModal, setOpenUrlModal] = useState(false); // 모달 창 열림 상태 관리
+  const [openUrlModal, setOpenUrlModal] = useState(false); // UrlGroup 창 열림/닫힘 관리 상태 변수
 
 
   // return이 있는 부분이 실제로 화면에서 보이는 부분
   return (
     // css 클래스 registration 적용
     <div className='registration'>
-        {/* 버튼 클릭 시 각 모달 창 열림 상태를 true로 변경하여 모달 창 표시 */}
+        {/* 버튼 클릭 시 각 group 모달 창 열림 상태를 true로 변경하여 창 내용 표시 */}
         
         {/* User Groups 버튼과 모달 창 */}
         <button className={props.mode === 'dark' ? 'dark-button' : 'light-button'} style={{marginTop:"100px"}} onClick={() => {setOpenModal(true);}}>User Groups</button>
-        {openModal && <Usermodal closeModal={setOpenModal} mode={props.mode}/>} 
+        {/* 버튼 클릭시 openModal이 true가 되어 Usermodal 컴포넌트 렌더링(보여줌) */}
+        {openModal && <Usermodal closeModal={setOpenModal} mode={props.mode}/>}
         
         {/* Device Groups 버튼과 모달 창 */}
         <button className={props.mode === 'dark' ? 'dark-button' : 'light-button'} onClick={() => {setOpenDeviceModal(true);}}>Device Groups</button>
+        {/* 버튼 클릭시 openDeviceModal이 true가 되어 Devicemodal 컴포넌트 렌더링(보여줌) */}
         {openDeviceModal && <Devicemodal closeDeviceModal={setOpenDeviceModal} mode={props.mode}/>}
 
         {/* Location Groups 버튼과 모달 창 */}
         <button className={props.mode === 'dark' ? 'dark-button' : 'light-button'}  onClick={() => {setOpenLocationModal(true);}}>Location Groups</button>
+        {/* 버튼 클릭시 openLocationModal이 true가 되어 Locationmodal 컴포넌트 렌더링(보여줌) */}
         {openLocationModal && <Locationmodal closeLocationModal={setOpenLocationModal} mode={props.mode}/>}
 
         {/* URL Groups 버튼과 모달 창 */}
         <button className={props.mode === 'dark' ? 'dark-button' : 'light-button'} onClick={() => {setOpenUrlModal(true);}}>URL Groups</button>
+        {/* 버튼 클릭시 openUrlModal이 true가 되어 Urlmodal 컴포넌트 렌더링(보여줌) */}
         {openUrlModal && <Urlmodal closeUrlModal={setOpenUrlModal} mode={props.mode}/>}
     </div>
   )
