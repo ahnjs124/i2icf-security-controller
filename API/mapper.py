5085b2b27298fdee85b114f9afa6d4f3d430c522
ahnjs124 <coffeecat432@gmail.com>
2025-09-22 19:03:48 +0900

    refactor: enhance comments for clarity in parsing and mapAttributes functions

--- API/mapper.py
+++ API/mapper.py
@@ -49,23 +49,40 @@ class yangdm:
             return "/{}".format(self.tag)
         else:
             return "{}/{}".format(self.parent.path(),self.tag)
-        
+
+
+
+
 #Parsing from YANG Tree Data Model into Python Class yangdm. Reading each line of the YANG Tree Data Model
+# YANG DataModel 파일의 각 line별 parsing
 def parsing(line,id):
     linelen = len(line)
     skip = False
     
+    # YANG 데이터모델 parsing
+    # 현재칸이 빈칸'' 혹은 |로 되어있을 때, +가 나올때까지 오른쪽으로 이동
+    # 예시:
+    #start(index): 2, level: 0
+    #  +--rw i2nsf-cfi-policy* [name]
+    #start(index): 5, level: 1
+    #  |  +--rw name                   string
+    #start(index): 17, level: 5
+    #  |     |  |     +--rw flow-rate-threshold?     uint64
     start = 0
     while line[start] == ' ' or line[start] == '|':
         start += 1
+
+    # YANG데이터모델은 level(depth)이 1단계 증가할 때마다 3칸씩 안쪽으로 들여쓰기 된다.
     level = int((start-2)/3)
     
+    # start(index)위치에서 3칸 옆이 r이 나옴. (+--rw)
     if line[start+3] == 'r':
-        start += 6
+        start += 6 # +로부터 데이터 시작부분은 6칸 옆에 나옴 (+--rw 데이터)
     else:
-        start += 4
+        start += 4 # 그 외의 데이터는 4칸 옆에 나옴 (+--:데이터)
     end = start
     
+
     if line[start] == '(':
         for i in range(start, linelen):
             if line[i] == ')':
@@ -87,12 +104,24 @@ def parsing(line,id):
             
     isLeaf=True
     end = start
+
+
     while line[end] ==' ':
         end += 1
+    
     if line[end] == '[' or line[end]=='\n':
         isLeaf = False
+
+    
     data = yangdm(tag=tag,id=id,level=level,isLeaf=isLeaf, skip=skip)
+    #print(data)
+    #print(data.printDM())
+
     return data
+
+
+
+
 
 def text_to_vector(text):
     WORD = re.compile(r"\w+")
@@ -144,21 +173,89 @@ class WeirdNode(object):
     
     
 def mapAttributes(cfiTree,nfiTree):
+
     # READ CFI / High-Level YANG Data Model tree and parse it into python yang dm class
     with open(cfiTree,'r') as f:
-        next(f)
+        next(f) # 다음줄로 넘어감 (1번째 줄 버림)
         id = 0
         cfiFull = [] #Full data model
+
+        # parsing(line,id)하면 이렇게 나타난다 = <mapper.yangdm object at 0x7f8d50323310>
+        # 그리고 parsing(line,id).printDM()을 하면, 내부 내용이 나온다.
+        # <mapper.yangdm object at 0x7f8d50323310>
+        # Tag    : group-name
+        # ID     : 189
+        # isLeaf : True
+        # Level  : 3
+        # Skip   : False
+        # Child  : []
+
+        # txt파일에서 한줄씩 가져오기
         for line in f:
-            cfiFull.append(parsing(line,id))
-            id += 1
-            
-    for i in range(len(cfiFull)-1,-1,-1):
+            cfiFull.append(parsing(line,id)) # cfiFull = [<mapper.yangdm object at 0x7f8d50323310>, <mapper.yangdm object at 0x7f8d50323370>, ...]
+            id += 1            
+
+    
+    
+    ##### cfiFull에 들어있는 모든 yang data model 데이터들의 부모값을 입력
+    # range(len(cfiFull)-1, -1, -1)
+    # 마지막 인덱스부터 0 인덱스까지 1씩 감소함
+    # cfiFull: [<mapper.yangdm object at 0x7f44fbfbb850>, <mapper.yangdm object at 0x7f44fbfbbfd0>, <mapper.yangdm object at 0x7f44fbfbb880>,....]
+    
+    # 예시:
+    '''
+    |     |  +--rw context
+    |     |  |  +--rw time
+    |     |  |  |  +--rw start-date-time?   yang:date-and-time
+    '''
+    # start-date-time의 부모는 time이고, time의 부모는 context이다.   
+
+    for i in range(len(cfiFull)-1,-1,-1): # i = 10, 9, 8, .... , 0
+
+        # cfi_minus.txt파일을 가장 아래줄부터 위로 훝는다.
+        # 기준행인 i행을 잡고, 그보다 위쪽에 있는 행등을 살피면서, 한칸 안쪽으로 들어간 행이 바로 부모로 설정한다.
+        # 즉, 기준 i행과 level을 비교해서 낮은 레벨의 행이 나오면 바로 부모로 설정한다.
+        print("YangDataModel line:")
+        print(cfiFull[i].printDM())
+
         for j in range(i-1,-1,-1):
+            # 부모를 찾기 위해 선택된 i행부터 위로 올라가면서, 자신과 같은 레벨은 지나치고,
+            # 한 칸 안쪽으로 들어간 행이 나오면 바로 그걸 부모 행으로 삼는다.
             if cfiFull[i].level > cfiFull[j].level:
                 cfiFull[i].setParent(cfiFull[j])
                 break
-    
+        
+        print("line parent:")
+        if i != 0:
+            print(cfiFull[i].parent.tag) # 부모행 tag
+        else: 
+            print(cfiFull[i].parent)
+        print("--------------")
+
+
+
+    ##### 건너뛸 부분(skip) 판단하기
+    # 위에서 각 행별로 parsing(line,id)를 해서, skip = false로 되어있음
+    # +--:(괄호내용) 이렇게 되어 있는 부분은 건너뛴다. (skip 부분)
+    # cfi_minus.txt에는 skip부분이 없고, nfi.txt에는 skip 부분이 존재함
+
+    # 즉, 부모 판단시, level이 낮아도 (괄호)처럼 된 부분이 있으면
+    # 그 부분은 건너뛰고 그보다 더 낮은 레벨을 부모로 삼는다.
+
+    # 예시:
+    '''
+     |  |  |  |  +--rw ipv4
+     |  |  |  |     +--rw dscp?                             inet:dscp
+     |  |  |  |     +--rw identification?                   uint16
+     |  |  |  |     +--rw (destination-network)?
+     |  |  |  |     |  +--:(destination-ipv4-network)
+     |  |  |  |     |  |  +--rw destination-ipv4-network?   inet:ipv4-prefix
+     |  |  |  |     +--rw (source-network)?
+     |  |  |  |        +--:(source-ipv4-network)
+     |  |  |  |        |  +--rw source-ipv4-network?        inet:ipv4-prefix
+    '''
+    # 여기서 source-ipv4-network의 바로 위쪽은 (괄호)로 되어있으므로,
+    # 그 부분은 skip을 해서, 부모는 ipv4 가 된다.
     for i in range(len(cfiFull)-1,-1,-1):
         try:
             while cfiFull[i].parent.skip:
@@ -166,6 +263,8 @@ def mapAttributes(cfiTree,nfiTree):
         except:
             pass
     
+
+    ##### 부모 아래의 직속 자식들을 확인한다.
     for i in range(len(cfiFull)):
         for j in range(len(cfiFull)):
             try:
@@ -174,8 +273,17 @@ def mapAttributes(cfiTree,nfiTree):
             except:
                 pass
                     
+
+        # i행 부모의 직속 자식만 즉시 확인
+        if cfiFull[i].child:
+            print(f"parent: {cfiFull[i].tag} (id={cfiFull[i].id}, path={cfiFull[i].path()})")
+            kids = [(ch.tag, ch.id, ch.path()) for ch in cfiFull[i].child]
+            print("  children:", kids)
+            print()
+
+
+
     cfiLeaf = [] #For only the leaf data model
-    
     leafID=0
     for x in cfiFull:
         if x.isLeaf and not x.skip:
@@ -188,6 +296,9 @@ def mapAttributes(cfiTree,nfiTree):
         if not x.isLeaf and not x.skip:
             cfiNonLeaf.append(x)
             
+
+
+
     # READ NFI / High-Level YANG Data Model tree and parse it into python yang dm class
     with open(nfiTree,'r') as f:
         next(f)
@@ -218,6 +329,9 @@ def mapAttributes(cfiTree,nfiTree):
             except:
                 pass
                     
+
+
+
     nfiLeaf = [] #For only the leaf data model
     nfiLeafID = 0
     for x in nfiFull:
@@ -230,6 +344,8 @@ def mapAttributes(cfiTree,nfiTree):
     for x in nfiFull:
         if not x.isLeaf and not x.skip:
             nfiNonLeaf.append(x)
+
+
 
     #Separating the YANG Tree into 1 branch
     #Edge Contraction also used here --> If the parent and the leaf has similar label, the edge is deleted, then the parent and the child is unified
@@ -272,6 +388,8 @@ def mapAttributes(cfiTree,nfiTree):
                     allChild.append(l)
         return allChild
     
+
+
     #MAPPING THE NON LEAF
     parentMap = {}
     for w in range(len(cfiNonLeaf)):
